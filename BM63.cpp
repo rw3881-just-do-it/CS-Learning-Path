c3c864f78e55d7befbdeb1fba4d6b366c3d54943
RW <rw3881@nyu.edu>
2026-03-11 18:39:56 -0400

    #Niuke #classic climbing stairs

---
+++ BM63.cpp
@@ -0,0 +1,20 @@
+/*total ways to go upstairs*/
+#include <iostream>
+using namespace std;
+int jumpFloor(int number){
+    if(number == 1){
+        return 1;
+    }
+    int prev = 1;
+    int next = 1;
+    int curr = 0;
+    for (int i = 2; i <= number;++i){
+        curr = prev + next;
+        prev = next;
+        next = curr;
+    }
+    return curr;
+}
+int main(){
+    cout << jumpFloor(7) << endl;
+}
